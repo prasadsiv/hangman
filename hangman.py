d60073f51b40e5715dbf48a04b9097a162793314
Sivaprasad <sivaprasad.g@live.com>
2021-07-10 10:57:14 +0530

    finished firstphase

---
+++ hangman.py
@@ -0,0 +1,14 @@
+import random 
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+guess = input("Guess a letter \n").lower()
+
+for char in  chosen_word:
+    if char == guess:
+        print("Right ")
+    else:
+        print("Wrong ")
+
+ 
