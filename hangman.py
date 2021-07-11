b320b390de01427506c438b164cb36faea4b751f
Sivaprasad <sivaprasad.g@live.com>
2021-07-11 21:24:49 +0530

    add end_of_game variable

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
@@ -9,11 +10,20 @@ display = []
 for _ in chosen_word:
     display+= "-"
 
-while  "-" in display:
+end_of_game = False
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
+
+  #check gussed letter
     for position in range(len(chosen_word)):
         if chosen_word[position] == guess:
             display[position] = guess
-    print(display)
+            if "_" not in display:
+                print("You win")
 
-print(display)
+    print(display)
+    
+    if "-" not in display:
+        end_of_game =True
+        print("You Win.")
+        
