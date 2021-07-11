aaf36dc5968a15606397a9fbb08d13a91e74c206
Sivaprasad <sivaprasad.g@live.com>
2021-07-11 17:22:43 +0530

    Stage 3

--- hangman.py
+++ hangman.py
@@ -1,7 +1,7 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
+word_length = len(chosen_word)
 #Testing code
 print(f"choosen word {chosen_word}.")
 
@@ -9,11 +9,11 @@ display = []
 for _ in chosen_word:
     display+= "-"
 
-guess = input("Guess a letter: ").lower()
-
-
-for position in range(len(chosen_word)):
-    if chosen_word[position] == guess:
-        display[position] = guess
+while  "-" in display:
+    guess = input("Guess a letter: ").lower()
+    for position in range(len(chosen_word)):
+        if chosen_word[position] == guess:
+            display[position] = guess
+    print(display)
 
 print(display)
